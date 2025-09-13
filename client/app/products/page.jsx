f5dc42edc39363383d2fba4d47660927384027e5
unknown <osemensilas@gmail.com>
2025-09-13 16:32:33 +0100

    New Commit

--- client/app/products/page.jsx
+++ client/app/products/page.jsx
@@ -386,6 +386,221 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
+                <div className={styles.productCard}>
+                    <div className={styles.productCardTop}>
+                        <Image src={"/"} className={styles.productImg} fill />
+                    </div>
+                    <div className={styles.productCardBottom}>
+                        <div className={styles.productCartPriceContainer}>
+                            <h2>₦{Number(145999).toLocaleString()}</h2>
+                            <p>In Stock</p>
+                        </div>
+                        <div className={styles.productCardName}>
+                            <h2>Lavender</h2>
+                        </div>
+                        <div className={styles.productCardReview}>
+                            <div className={styles.productCardReviewItem}>
+                                <div className={styles.productCardStars}>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                </div>
+                                <p>7.0</p>
+                            </div>
+                            <Link href={"/"}><i className="fa fa-arrow-right"></i> Add</Link>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.productCard}>
+                    <div className={styles.productCardTop}>
+                        <Image src={"/"} className={styles.productImg} fill />
+                    </div>
+                    <div className={styles.productCardBottom}>
+                        <div className={styles.productCartPriceContainer}>
+                            <h2>₦{Number(145999).toLocaleString()}</h2>
+                            <p>In Stock</p>
+                        </div>
+                        <div className={styles.productCardName}>
+                            <h2>Lavender</h2>
+                        </div>
+                        <div className={styles.productCardReview}>
+                            <div className={styles.productCardReviewItem}>
+                                <div className={styles.productCardStars}>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                </div>
+                                <p>7.0</p>
+                            </div>
+                            <Link href={"/"}><i className="fa fa-arrow-right"></i> Add</Link>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.productCard}>
+                    <div className={styles.productCardTop}>
+                        <Image src={"/"} className={styles.productImg} fill />
+                    </div>
+                    <div className={styles.productCardBottom}>
+                        <div className={styles.productCartPriceContainer}>
+                            <h2>₦{Number(145999).toLocaleString()}</h2>
+                            <p>In Stock</p>
+                        </div>
+                        <div className={styles.productCardName}>
+                            <h2>Lavender</h2>
+                        </div>
+                        <div className={styles.productCardReview}>
+                            <div className={styles.productCardReviewItem}>
+                                <div className={styles.productCardStars}>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                </div>
+                                <p>7.0</p>
+                            </div>
+                            <Link href={"/"}><i className="fa fa-arrow-right"></i> Add</Link>
+                        </div>
+                    </div>
+                </div><div className={styles.productCard}>
+                    <div className={styles.productCardTop}>
+                        <Image src={"/"} className={styles.productImg} fill />
+                    </div>
+                    <div className={styles.productCardBottom}>
+                        <div className={styles.productCartPriceContainer}>
+                            <h2>₦{Number(145999).toLocaleString()}</h2>
+                            <p>In Stock</p>
+                        </div>
+                        <div className={styles.productCardName}>
+                            <h2>Lavender</h2>
+                        </div>
+                        <div className={styles.productCardReview}>
+                            <div className={styles.productCardReviewItem}>
+                                <div className={styles.productCardStars}>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                </div>
+                                <p>7.0</p>
+                            </div>
+                            <Link href={"/"}><i className="fa fa-arrow-right"></i> Add</Link>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.productCard}>
+                    <div className={styles.productCardTop}>
+                        <Image src={"/"} className={styles.productImg} fill />
+                    </div>
+                    <div className={styles.productCardBottom}>
+                        <div className={styles.productCartPriceContainer}>
+                            <h2>₦{Number(145999).toLocaleString()}</h2>
+                            <p>In Stock</p>
+                        </div>
+                        <div className={styles.productCardName}>
+                            <h2>Lavender</h2>
+                        </div>
+                        <div className={styles.productCardReview}>
+                            <div className={styles.productCardReviewItem}>
+                                <div className={styles.productCardStars}>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                </div>
+                                <p>7.0</p>
+                            </div>
+                            <Link href={"/"}><i className="fa fa-arrow-right"></i> Add</Link>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.productCard}>
+                    <div className={styles.productCardTop}>
+                        <Image src={"/"} className={styles.productImg} fill />
+                    </div>
+                    <div className={styles.productCardBottom}>
+                        <div className={styles.productCartPriceContainer}>
+                            <h2>₦{Number(145999).toLocaleString()}</h2>
+                            <p>In Stock</p>
+                        </div>
+                        <div className={styles.productCardName}>
+                            <h2>Lavender</h2>
+                        </div>
+                        <div className={styles.productCardReview}>
+                            <div className={styles.productCardReviewItem}>
+                                <div className={styles.productCardStars}>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                </div>
+                                <p>7.0</p>
+                            </div>
+                            <Link href={"/"}><i className="fa fa-arrow-right"></i> Add</Link>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.productCard}>
+                    <div className={styles.productCardTop}>
+                        <Image src={"/"} className={styles.productImg} fill />
+                    </div>
+                    <div className={styles.productCardBottom}>
+                        <div className={styles.productCartPriceContainer}>
+                            <h2>₦{Number(145999).toLocaleString()}</h2>
+                            <p>In Stock</p>
+                        </div>
+                        <div className={styles.productCardName}>
+                            <h2>Lavender</h2>
+                        </div>
+                        <div className={styles.productCardReview}>
+                            <div className={styles.productCardReviewItem}>
+                                <div className={styles.productCardStars}>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                </div>
+                                <p>7.0</p>
+                            </div>
+                            <Link href={"/"}><i className="fa fa-arrow-right"></i> Add</Link>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.productCard}>
+                    <div className={styles.productCardTop}>
+                        <Image src={"/"} className={styles.productImg} fill />
+                    </div>
+                    <div className={styles.productCardBottom}>
+                        <div className={styles.productCartPriceContainer}>
+                            <h2>₦{Number(145999).toLocaleString()}</h2>
+                            <p>In Stock</p>
+                        </div>
+                        <div className={styles.productCardName}>
+                            <h2>Lavender</h2>
+                        </div>
+                        <div className={styles.productCardReview}>
+                            <div className={styles.productCardReviewItem}>
+                                <div className={styles.productCardStars}>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                </div>
+                                <p>7.0</p>
+                            </div>
+                            <Link href={"/"}><i className="fa fa-arrow-right"></i> Add</Link>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
