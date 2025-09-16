af85e7137f1cda1f0b8e7eea07329461e86d1f3a
unknown <osemensilas@gmail.com>
2025-09-16 12:36:09 +0100

    New Commit

---
+++ client/app/cart/page.jsx
@@ -0,0 +1,15 @@
+'use client'
+
+import styles from '../css/cart.module.css';
+
+const Page = () => {
+    return ( 
+        <>
+        <div className={styles.page}>
+
+        </div>
+        </>
+     );
+}
+ 
+export default Page;
