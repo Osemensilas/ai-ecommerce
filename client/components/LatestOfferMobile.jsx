45740da30b243924c29d454f2c6bc53fb1440849
solohitech001 <solohitech001@gmail.com>
2025-09-25 09:16:02 +0100

    vercel commit

--- client/components/LatestOfferMobile.jsx
+++ client/components/LatestOfferMobile.jsx
@@ -23,19 +23,7 @@ const products = [
 export default function LatestOfferMobile() {
     const isMobile = useMediaQuery("(max-width:768px)");
 
-    // const settings = {
-    //     dots: true,
-    //     infinite: true,
-    //     speed: 500,
-    //     slidesToShow: 4,
-    //     slidesToScroll: 1,
-    //     centerPadding: "0px",
-    //     responsive: [
-    //         { breakpoint: 1024, settings: { slidesToShow: 3, slidesToScroll: 1 } },
-    //         { breakpoint: 768, settings: { slidesToShow: 2, slidesToScroll: 1 } },
-    //         { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } },
-    //     ],
-    // };
+
 
     return (
         <div style={{ padding: isMobile ? "0.5rem" : "3rem", backgroundColor: "", marginTop: isMobile ? "1rem" : "-2rem" }}>
