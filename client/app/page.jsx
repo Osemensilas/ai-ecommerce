ddf86da53fb410a6707f07b18b139d8602755f09
unknown <osemensilas@gmail.com>
2025-10-07 06:28:06 +0100

    New Commit

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,15 +1,18 @@
 import CTA from "@/components/CTA";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Suspense } from 'react';
 
 
 export default function Home() {
   return (
     <>
     <Header />
-    <section id="hompage">
-      <CTA />
-    </section>
+    <Suspense fallback={<div>Loading...</div>}>
+      <section id="hompage">
+        <CTA />
+      </section>
+    </Suspense>
     <Footer />
     </>   
   );
