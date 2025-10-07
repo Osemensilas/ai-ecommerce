6155ac1a90dc6299b7130aadd6dc4c315256109b
unknown <osemensilas@gmail.com>
2025-10-07 07:54:39 +0100

    New Commit

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Head from "next/head";
 
 export const metadata = {
   title: "E commerce",
@@ -10,12 +9,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-        <link rel="stylesheet" href="fa/fontawesome-free-6.6.0-web/css/all.css" />
-      </Head>
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
+      </head>
       <body>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- client/components/settingsComponents/PersonalInfo.jsx
+++ client/components/settingsComponents/PersonalInfo.jsx
@@ -1,7 +1,33 @@
+import styles from '../../app/css/dashboard.module.css';
+import Image from 'next/image';
+
 const PersonalIfo = () => {
     return ( 
         <>
-        <h2>Personal Info</h2>
+        <div className={styles.settingLeftMainContainer}>
+            <div className={styles.settingPersonalForm}>
+                <div className={styles.settingPersonalFormTop}>
+                    <div className={styles.settingPersonalFormTopLeft}>
+                        <div className={styles.settingPersonalFormImgContainer}>
+                            <Image src="" fill alt="user image" className={styles.settingsPersonalImg} />
+                        </div>
+                    </div>
+                    <div className={styles.settingPersonalFormTopRight}>
+                        <h3>Solo-HiTec</h3>
+                        <div className={styles.settingPersonalRow}>
+                            <div className={styles.settingPersonalRowCont}>
+                                <h3>Seller</h3>
+                                <p>w457e5989</p>
+                            </div>
+                            <i className="fa-solid fa-files"></i>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.settingPersonalFormBottom}>
+
+                </div>
+            </div>
+        </div>
         </>
      );
 }
