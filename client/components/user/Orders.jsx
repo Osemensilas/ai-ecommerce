36ab975ef4160b1aad12d0176d246543e3cc7434
unknown <osemensilas@gmail.com>
2025-10-29 20:13:53 +0100

    New COmmit

--- client/components/user/Orders.jsx
+++ client/components/user/Orders.jsx
@@ -19,10 +19,60 @@ const Orders = () => {
                     </div>
                     <div className={styles.userOrderCardBottom}>
                         <div className={styles.userOrderCardBottomLeft}>
-
+                            <div className={styles.userOrderCardImgContainer}>
+                                <Image src={"/hair1.png"} fill className={styles.userOrderCardImg} />
+                            </div>
+                            <div className={styles.userOrderCardDesContainer}>
+                                <h3>Order Confirmed</h3>
+                                <h2>22' Human Hair</h2>
+                                <p>Purchased Feb - 25</p>
+                            </div>
                         </div>
                         <div className={styles.userOrderCardBottomRight}>
-
+                            <button type="button" className={styles.userOrderCardBtnAction}>Track Order</button>
+                            <button type="button" className={styles.userOrderCardBtnAction}>. . .</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.userOrderCard}>
+                    <div className={styles.userOrderCardTop}>
+                        <p>Jan 18 - Oct 14</p>
+                    </div>
+                    <div className={styles.userOrderCardBottom}>
+                        <div className={styles.userOrderCardBottomLeft}>
+                            <div className={styles.userOrderCardImgContainer}>
+                                <Image src={"/hair1.png"} fill className={styles.userOrderCardImg} />
+                            </div>
+                            <div className={styles.userOrderCardDesContainer}>
+                                <h3>Order Confirmed</h3>
+                                <h2>22' Human Hair</h2>
+                                <p>Purchased Feb - 25</p>
+                            </div>
+                        </div>
+                        <div className={styles.userOrderCardBottomRight}>
+                            <button type="button" className={styles.userOrderCardBtnAction}>Track Order</button>
+                            <button type="button" className={styles.userOrderCardBtnAction}>. . .</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.userOrderCard}>
+                    <div className={styles.userOrderCardTop}>
+                        <p>Jan 18 - Oct 14</p>
+                    </div>
+                    <div className={styles.userOrderCardBottom}>
+                        <div className={styles.userOrderCardBottomLeft}>
+                            <div className={styles.userOrderCardImgContainer}>
+                                <Image src={"/hair1.png"} fill className={styles.userOrderCardImg} />
+                            </div>
+                            <div className={styles.userOrderCardDesContainer}>
+                                <h3>Order Confirmed</h3>
+                                <h2>22' Human Hair</h2>
+                                <p>Purchased Feb - 25</p>
+                            </div>
+                        </div>
+                        <div className={styles.userOrderCardBottomRight}>
+                            <button type="button" className={styles.userOrderCardBtnAction}>Track Order</button>
+                            <button type="button" className={styles.userOrderCardBtnAction}>. . .</button>
                         </div>
                     </div>
                 </div>
