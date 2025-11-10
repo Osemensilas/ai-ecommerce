c97fd95b5a11c97d559e0b44ad1b58dc7c42cce5
solohitech001 <solohitech001@gmail.com>
2025-11-11 00:01:11 +0100

    solo

--- client/components/BannerCarousel.jsx
+++ client/components/BannerCarousel.jsx
@@ -8,6 +8,37 @@ import { useMediaQuery } from "@mui/material";
 const banners = [
   {
     id: 1,
+    image: "/ahiaglobalgrocery.png",
+    alt: "Special Deals Banner 1",
+  },
+  {
+    id: 1,
+    image: "/ahiaglobalmaternity.png",
+    alt: "Special Deals Banner 1",
+  },
+  {
+    id: 1,
+    image: "/ahiaglobalphones.png",
+    alt: "Special Deals Banner 1",
+  },
+  {
+    id: 1,
+    image: "/ahiaglobalhomeappliances.png",
+    alt: "Special Deals Banner 1",
+  },
+
+  {
+    id: 1,
+    image: "/ahiaglobalgaming.png",
+    alt: "Special Deals Banner 1",
+  },
+  {
+    id: 1,
+    image: "/ahiaglobalcomputers.png",
+    alt: "Special Deals Banner 1",
+  },
+  {
+    id: 1,
     image: "/Ahiaglobal1.jpg",
     alt: "Special Deals Banner 1",
   },
@@ -18,14 +49,31 @@ const banners = [
   },
   {
     id: 3,
-    image: "AhiaGlobal.png",
+    image: "AhiaGlobal0.png",
     alt: "Special Deals Banner 3",
   },
   {
     id: 0,
+    image: "/ahiaglobal-.png",
+    alt: "Special Deals Banner 1",
+  },
+  {
+    id: 2,
+    image: "ahiaglobal-2.png",
+    alt: "Special Deals Banner 2",
+  },
+  {
+    id: 3,
+    image: "ahiagloball.png",
+    alt: "Special Deals Banner 3",
+  },
+
+  {
+    id: 1,
     image: "/ahiaglobal0.png",
     alt: "Special Deals Banner 1",
   },
+
 ];
 
 export default function BannerCarousel() {
@@ -38,7 +86,7 @@ export default function BannerCarousel() {
     slidesToShow: isMobile ? 1 : 2,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 1000,
+    autoplaySpeed: 1070, 
     arrows: true,
   };
 
